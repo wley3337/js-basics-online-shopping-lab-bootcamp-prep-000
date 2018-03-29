cd56681cfb51d11c181522f6b2e13df3c80f7689
wley3337@gmail.com <wley3337@gmail.com>
2018-03-29 17:25:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,37 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  cart.push(Object.assign({},{ itemName: `${item}`},{itemPrice: `${Math.floor(100*Math.random())}`}));
 }
+
 
 function viewCart() {
   // write your code here
+  //In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.
+  if (cart.length === 0){
+    return "Your Shopping cart is empty.";
+  } else {
+  //for loop that recalls each object and places it into the string In your cart, you have      . 
+      
+     var tempCart = Ojbect.assign({}, cart);
+      
+     var viewCartList = [];
+      
+     for (var i = 1; i < tempCart.length; i++){ 
+       
+       viewCartList.push(
+      ` ${Object.key(tempCart)[${i-1}]} at ${Object.values(tempCart)[${i-1}]}`
+      );
+     }
+     for (; i=== tempCart.length;i++) {viewCartList.push(`${tempCart.shift()}.`);
 }
+      return `In your cart, you have${viewCartList}.`;
+    
+    }
+      
+
+//   }
+// }
 
 function total() {
   // write your code here
